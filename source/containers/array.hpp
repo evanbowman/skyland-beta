fa5ea1574e1b74904b8e0d3918c3cda674177678
Evan Bowman <evanbowman@Evans-MacBook-Air.local>
2024-02-03 10:52:32 -0500

    add new container headers

---
+++ source/containers/array.hpp
@@ -0,0 +1,75 @@
+#pragma once
+
+
+template <typename T, unsigned int count>
+class Array
+{
+public:
+
+
+    constexpr auto capacity() const
+    {
+        return count;
+    }
+
+
+    constexpr auto size() const
+    {
+        return count;
+    }
+
+
+    constexpr T& operator[](unsigned int index)
+    {
+        return data_[index];
+    }
+
+
+    constexpr const T& operator[](unsigned int index) const
+    {
+        return data_[index];
+    }
+
+
+    constexpr T* data()
+    {
+        return data_;
+    }
+
+
+    constexpr const T* data() const
+    {
+        return data_;
+    }
+
+
+    using iterator = T*;
+
+    constexpr iterator begin()
+    {
+        return data_;
+    }
+
+
+    constexpr const T* begin() const
+    {
+        return data_;
+    }
+
+
+    constexpr iterator end()
+    {
+        return data_ + count;
+    }
+
+
+    constexpr const T* end() const
+    {
+        return data_ + count;
+    }
+
+
+
+private:
+    T data_[count];
+};
