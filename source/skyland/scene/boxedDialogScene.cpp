c9c2d6398d6fd5a56c2425aaa089e9c2d36cae33
Ophir Doron <tulidoron@gmail.com>
2024-11-26 16:32:10 +0200

    Update boxedDialogScene.cpp

--- source/skyland/scene/boxedDialogScene.cpp
+++ source/skyland/scene/boxedDialogScene.cpp
@@ -883,8 +883,8 @@ ScenePtr
 dialog_prompt(SystemString systr, DeferredScene next, const char* ambience)
 {
     lisp::set_var("on-dialog-closed", L_NIL);
-    PLATFORM.screen().fade(0.95f);
-    PLATFORM.screen().fade(1.f);
+    PLATFORM.screen().schedule_fade(0.95f);
+    PLATFORM.screen().schedule_fade(1.f);
     auto dialog = allocate_dynamic<DialogString>("dialog-buffer");
     *dialog = loadstr(systr)->c_str();
     auto s = make_scene<BoxedDialogScene>(std::move(dialog));
