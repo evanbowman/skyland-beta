09097c4c8abebc436264841092cb093ac07a3ea7
Extloga <141232749+Extloga@users.noreply.github.com>
2025-07-27 19:39:33 +0200

    Update dec-revenge.lisp

--- scripts/event/hostile/dec-revenge.lisp
+++ scripts/event/hostile/dec-revenge.lisp
@@ -6,7 +6,7 @@
   (dialog
    (if (room-count (player) 'decimator)
        "<c:goblin_captain:43>There! That'sss our decimator! <B:0> We've been hunting that weapon for weeksss! <B:0> No talksss, no dealsss - we're taking it back! <B:0> ATTACK!"
-       "<c:goblin_captain:43>You! We know you have our ssstolen decimator! <B:0> Don't try to hide it - we tracked the energy ssignature to YOUR ship! <B:0> Where isss it?! No matter... we'll tear your castle apart until we find it! <B:0> ATTACK!")))
+       "<c:goblin_captain:43>You! We know you have our ssstolen decimator! <B:0> Don't try to hide it - we tracked the energy sssignature to YOUR ship! <B:0> Where isss it?! No matter... we'll tear your castle apart until we find it! <B:0> ATTACK!")))
 
 
 (adventure-log-add 75 '())
