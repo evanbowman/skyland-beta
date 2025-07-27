dd2261938b1d3cf36b88e92bfc30c0735a0652f9
Extloga <141232749+Extloga@users.noreply.github.com>
2025-07-27 22:00:28 +0200

    Update 6.lisp

--- scripts/event/quest/goblin/6.lisp
+++ scripts/event/quest/goblin/6.lisp
@@ -17,15 +17,15 @@
 
   (defn on-dialog-closed ()
     (dialog "<c:injured boy:26> "
-            "<S:1>i am the only survivor! can you help me get back home?")
+            "<S:1>I am the only survivor! Can you help me get back home?")
 
     (defn on-dialog-closed ()
       (dialog "You can't understand a word he's saying. But he seems to want to join your crew.<B:0> Invite him aboard?")
 
       (setq on-dialog-closed nil)
 
-      (dialog-await-binary-q-w/lore "yes" "I'll pass"
-                                    '(("who might he be?" .
+      (dialog-await-binary-q-w/lore "Yes." "I'll pass."
+                                    '(("Who might he be?" .
                                        "He looks like he might be a Sylph child. They've been up here a long time, but not much is known about them. <B:0> Invite him aboard?")))
 
       (defn on-dialog-accepted ()
