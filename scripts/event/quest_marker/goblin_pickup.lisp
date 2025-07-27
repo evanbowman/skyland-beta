1d3b71c9568b81c854c4754b398db8f08e81c904
Extloga <141232749+Extloga@users.noreply.github.com>
2025-07-27 22:28:47 +0200

    Update goblin_pickup.lisp

--- scripts/event/quest_marker/goblin_pickup.lisp
+++ scripts/event/quest_marker/goblin_pickup.lisp
@@ -2,6 +2,7 @@
 ;;; quest_marker/goblin_pickup.lisp
 ;;;
 
+
 (if (chance 4)
     (eval-file "scripts/event/quest_marker/goblin_pickup_1.lisp")
   (eval-file "scripts/event/quest_marker/goblin_pickup_0.lisp"))
