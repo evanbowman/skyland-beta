1e5cc986e5b51eaf550f9faea9d29fdc9147f551
Extloga <141232749+Extloga@users.noreply.github.com>
2025-07-29 01:16:47 +0200

    Update title_screen_isle.lisp

--- scripts/config/title_screen_isle.lisp
+++ scripts/config/title_screen_isle.lisp
@@ -1,4 +1,6 @@
-
+;;
+;; title_screen_isle.lisp
+;;
 ;; The title screen islands are defined in title/. The game invokes this script
 ;; in the macrocosm room of the tile screen, and selects a random layout from
 ;; the title/ subdirectory. When adding new islands, modify the choice count
