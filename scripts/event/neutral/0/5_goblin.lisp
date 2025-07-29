831e4f08790175e09d606b5ea1fafb26e0c5c431
Extloga <141232749+Extloga@users.noreply.github.com>
2025-07-30 01:14:40 +0200

    Update 5_goblin.lisp

--- scripts/event/neutral/0/5_goblin.lisp
+++ scripts/event/neutral/0/5_goblin.lisp
@@ -40,7 +40,7 @@
 
 (defn on-converge ()
   (dialog
-   "<c:goblin raider:39>Finally, sssomeone answered! <B:0> My raid crew left me behind when the town's defenssses were stronger than expected. <B:0> Got knocked out by some debris when their ion cannon hit us... By the time I came to, everyone wasss gone. _bitter_laugh_ Guesss they figured I was dead weight. <B:0> Would you have room for a more... reliable crewmember?")
+   "<c:Goblin Raider:39>Finally, sssomeone answered! <B:0> My raid crew left me behind when the town's defenssses were stronger than expected. <B:0> Got knocked out by some debris when their ion cannon hit us... By the time I came to, everyone wasss gone. _bitter_laugh_ Guesss they figured I was dead weight. <B:0> Would you have room for a more... reliable crew member?")
 
   (setq on-dialog-closed
         (lambda ()
@@ -56,13 +56,13 @@
 (defn on-dialog-accepted ()
   (run-util-script
    "find-crew-slot"
-   "<c:goblin raider:39>Hold on, don't leave me here! I know your cassstle's full, but thisss island is burning!"
+   "<c:Goblin Raider:39>Hold on, don't leave me here! I know your cassstle's full, but thisss island is burning!"
    'ladder
    "Place block (1x2):"
    (lambda (x y _)
      (chr-del (opponent) 1 12)
      (chr-new (player) x y 'neutral '((race . 1) (icon . 39)))
-     (dialog "<c:goblin raider:39>Thanks for ressscuing me! I'll try to help out however I can!")
+     (dialog "<c:Goblin Raider:39>Thanks for ressscuing me! I'll try to help out however I can!")
      (defn on-dialog-closed ()
        (setq on-dialog-closed nil)
        (dialog "The goblin joined your crew!")
