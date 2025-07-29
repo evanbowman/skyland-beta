0afa44fcbcae1c874dafec986b8de64fbbd48baa
Extloga <141232749+Extloga@users.noreply.github.com>
2025-07-30 00:20:40 +0200

    Update atomics.lisp

--- scripts/event/quest_marker/atomics.lisp
+++ scripts/event/quest_marker/atomics.lisp
@@ -17,11 +17,11 @@
   (dialog "You arrive, but the princess looks troubled...")
 
   (defn on-dialog-closed ()
-    (dialog "<c:warrior princess of E. Cay:28>Since shortly after we departed, I haven't been able to contact anyone back home. <B:0> I have instructions, if our kingdom is overrun, to activate the atomics and use them to avenge my people's deaths. Please accept this:")
+    (dialog "<c:Warrior Princess of E. Cay:28>Since shortly after we departed, I haven't been able to contact anyone back home. <B:0> I have instructions, if our kingdom is overrun, to activate the atomics and use them to avenge my people's deaths. Please accept this:")
     (defn on-dialog-closed ()
       (dialog "[You write down the launch codes...]")
       (defn on-dialog-closed ()
-        (dialog "<c:warrior princess of E. Cay:28>Now, be very careful when moving them...")
+        (dialog "<c:Warrior Princess of E. Cay:28>Now, be very careful when moving them...")
         (defn on-dialog-closed ()
           (foreach (lambda (r)
                      (when (equal (car r) 'cloak)
