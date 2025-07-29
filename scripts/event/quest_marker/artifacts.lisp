d550a2d637efb27fc6608a8cff1bb2de87ee6249
Extloga <141232749+Extloga@users.noreply.github.com>
2025-07-30 00:19:47 +0200

    Update artifacts.lisp

--- scripts/event/quest_marker/artifacts.lisp
+++ scripts/event/quest_marker/artifacts.lisp
@@ -36,7 +36,7 @@
               c)))
       (if p
           (let ((sale (+ 1000 (* (lookup 4 qvar) 2))))
-            ;; Clear out cargo
+            ;; Clear out cargo.
             (cargo-set (player)
                        (caar p)
                        (cdr (car p))
@@ -44,7 +44,7 @@
 
             (dialog
              (format
-              "<c:the collector:6>Wow, such rare artifacts! I must have them!! How much would you sell them for? Here, take %@!"
+              "<c:The Collector:6>Wow, such rare artifacts! I must have them!! How much would you sell them for? Here, take %@!"
               sale))
 
             (coins-add sale)
