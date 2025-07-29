db8d3b9beff8221a46e41777059a5cefc9389c07
Extloga <141232749+Extloga@users.noreply.github.com>
2025-07-29 23:50:59 +0200

    Update hard.lisp

--- scripts/config/forever/hard.lisp
+++ scripts/config/forever/hard.lisp
@@ -10,7 +10,7 @@
 (foreach (lambda (kvp) (setvar (second kvp) (first kvp)))
  '((17 . "sf_p1_coin_yield")
    (14 . "sf_p2_coin_yield")
-   (7 . "sf_p3_coin_yield")
+   (7  . "sf_p3_coin_yield")
    (3  . "sf_p4_coin_yield")))
 
 (setvar "score_multiplier" 4)
