7b76447079c64cff6b5f24fc0f1dd59dab7bba59
Evan Bowman <evan.a.bowman@gmail.com>
2026-07-25 10:30:33 -0400

    raise incinerator damage slightly

--- scripts/config/damage.lisp
+++ scripts/config/damage.lisp
@@ -17,8 +17,8 @@
    (12  . "flak_r2_damage")
    (10  . "flak_r3_damage")
    (20  . "arcbolt_damage")
-   (10   . "incinerator_impact_damage")
-   (2   . "incinerator_splash_damage")
+   (14   . "incinerator_impact_damage")
+   (3   . "incinerator_splash_damage")
    (40  . "rocket_bomb_impact_damage")
    (16  . "rocket_bomb_splash_damage")))
 
