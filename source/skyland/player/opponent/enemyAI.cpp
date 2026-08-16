a570cfdfc34eca1cd8ab3b9fb6d9f7577ec5a87c
Evan Bowman <evan.a.bowman@gmail.com>
2026-08-16 16:17:41 -0400

    injured crew recovery bugfix

--- source/skyland/player/opponent/enemyAI.cpp
+++ source/skyland/player/opponent/enemyAI.cpp
@@ -374,8 +374,9 @@ void EnemyAI::update_room(Room& room,
                          it not_eq boarded_ai_characters.end();) {
                         if ((*it).first->health() < 25 and
                             not(*it).first->is_replicant()) {
+                            auto pos = (*it).first->grid_position();
                             it = boarded_ai_characters.erase(it);
-                            return (*it).first->grid_position();
+                            return pos;
                         } else {
                             ++it;
                         }
